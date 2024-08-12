61de09e039904410d91db36d479c6eeaf77f011e
Vaibhav Kumar Sharma <sumit.sharma290@gmail.com>
2024-08-12 23:22:09 +0530

    Chat App

---
+++ chat.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("WebSocket server is running...");
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Received: ${message}`);
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Chat server is running on port 3000");
+});
